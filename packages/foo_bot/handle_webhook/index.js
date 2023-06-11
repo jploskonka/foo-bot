1e1485c5238a3cd0393e4d1110007d4103562dd1
Jakub Płoskonka <ploskonkajakub@gmail.com>
2023-06-11 08:59:02 +0200

    for deploy with GPT interaction

--- packages/foo_bot/handle_webhook/index.js
+++ packages/foo_bot/handle_webhook/index.js
@@ -1,11 +1,34 @@
-// index.js
-function main(args) {
-  if(args.message) {
-    console.log(`message received with text: ${args.message.text}`)
-    return {"body": "OK"}
+const { Configuration, OpenAIApi } = require('openai')
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_TOKEN
+})
+
+async function main(args) {
+  if(!args.message) {
+    console.log('Request without message')
+    return {}
   }
 
-  return {}
+  const { text } = args.message
+  const openai = new OpenAIApi(configuration)
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'user', content: text }
+      ]
+    })
+
+    const { content } = completion.data.choices[0].message
+
+    console.log(`For message: ${text}; Reply from GPT: ${content}`)
+  } catch (err) {
+    console.error(err)
+  }
+
+  return {"body": "OK"}
 }
 
 exports.main = main
